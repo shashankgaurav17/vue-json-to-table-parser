0b028d2f6337bbda7bf7c052c470953b317ba6a7
Shashank Gaurav <shashank@cyware.com>
2020-02-13 19:50:45 +0530

    (refactor): app -> deleted unused v1 and v2 tables

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <span class="cursor-pointer" @click="type = 0">Table1</span>
-    <span class="cursor-pointer" @click="type = 1">Table2</span>
-    <Table v-if="type === 0" />
-    <Table2 v-if="type === 1" :data="data2" />
+    <Table2 :data="data2" />
   </div>
 </template>
 
 <script>
-import Table from "./components/Table.vue";
 import Table2 from "./components/Table2.vue";
-
 import * as data2 from "@/config/data2.json";
 
 export default {
   components: {
-    Table,
     Table2
   },
   data() {
     return {
-      type: 0,
       data2: data2.default
-    }
+    };
   }
 };
 </script>
