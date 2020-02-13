33152955b48b1221620117e118bfa8e4f089d175
Shashank Gaurav <shashank@cyware.com>
2020-02-13 19:23:54 +0530

    Added Stable Table Version3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
-    <Table />
+    <span class="cursor-pointer" @click="type = 0">Table1</span>
+    <span class="cursor-pointer" @click="type = 1">Table2</span>
+    <Table v-if="type === 0" />
+    <Table2 v-if="type === 1" :data="data2" />
   </div>
 </template>
 
 <script>
 import Table from "./components/Table.vue";
+import Table2 from "./components/Table2.vue";
+
+import * as data2 from "@/config/data2.json";
 
 export default {
   components: {
-    Table
+    Table,
+    Table2
+  },
+  data() {
+    return {
+      type: 0,
+      data2: data2.default
+    }
   }
 };
 </script>
@@ -22,5 +35,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.cursor-pointer {
+  cursor: pointer;
 }
 </style>
